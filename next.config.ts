47b4d59c2fd0a2a1060cf6744e30275aae07eec1
devang-garg <devang@moy-ka.com>
2025-05-30 18:17:15 +0530

    deploymnet fix

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,44 @@
 const nextConfig: import('next').NextConfig = {
-    // Recommended for static deployments
+  // Recommended for static deployments
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/onvi-sales-landing',
+  assetPrefix: '/onvi-sales-landing/',
+  trailingSlash: true,
 
-    async rewrites() {
-        return [
-            {
-                source: '/.well-known/:path*',
-                destination: '/.well-known/:path*',
-            },
-        ];
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/:path*',
+        destination: '/.well-known/:path*',
+      },
+    ];
+  },
 
-    async headers() {
-        return [
-            {
-                source: '/.well-known/apple-app-site-association',
-                headers: [
-                    {
-                        key: 'Content-Type',
-                        value: 'application/json',
-                    },
-                ],
-            },
-            {
-                source: '/.well-known/assetlinks.json',
-                headers: [
-                    { key: 'Content-Type', value: 'application/json' }
-                ],
-            }
-        ];
-    },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+      {
+        source: '/.well-known/assetlinks.json',
+        headers: [
+          { key: 'Content-Type', value: 'application/json' }
+        ],
+      }
+    ];
+  },
 };
 
 export default nextConfig;
